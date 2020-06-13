8385045bd0c192bfe9f228bd96250ae6cf8fdc53
John Kramer <enterprise1117@gmail.com>
2020-06-12 20:17:47 -0500

    added bot vs client and commands

--- bot.py
+++ bot.py
@@ -3,19 +3,68 @@
 import os
 import discord
 from dotenv import load_dotenv
+import random
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} is connected to the following guild:\n'
-          f'{guild.name}(id: {guild.id})'
-          )
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(
+        f'{bot.user.name} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
 
-client.run(TOKEN)
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+        'Noice. Smort.',
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+
+@bot.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+
+bot.run(TOKEN)
